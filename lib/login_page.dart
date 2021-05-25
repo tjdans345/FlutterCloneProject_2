52b86e86250d4499b158f6a4e69bf1cd43bf72a3
tjdans345 <tjdans345@naver.com>
2021-05-25 21:39:04 +0900

    Instagram Clone Project Ver.2 Start

---
+++ lib/login_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'tab_page.dart';
+
+class LoginPage extends StatelessWidget {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram Clone',
+              style: GoogleFonts.pacifico(
+                fontSize: 40.0,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TabPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
