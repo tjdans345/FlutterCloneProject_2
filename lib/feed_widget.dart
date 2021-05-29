03fd58b4064f09bbca323bac7e4ca09f731fd4ad
tjdans345 <tjdans34568@gmail.com>
2021-05-29 20:45:37 +0900

    좋아요 기능 완성 / 댓글 기능 구현중

--- lib/feed_widget.dart
+++ lib/feed_widget.dart
@@ -26,7 +26,7 @@ class _FeedWidgetState extends State<FeedWidget> {
 
   @override
   Widget build(BuildContext context) {
-    var comment = widget.document['comment'] ?? 0;
+    var commentCount = widget.document['comment'] ?? 0;
     return Column(
       children: <Widget>[
         ListTile(
@@ -49,7 +49,16 @@ class _FeedWidgetState extends State<FeedWidget> {
           leading: Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Icon(Icons.favorite_border),
+              widget.document['likeUsers']?.contains(widget.user.email) ?? false
+                  ? GestureDetector(
+                      onTap: _unlike,
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: _like, child: Icon(Icons.favorite_border)),
               SizedBox(
                 width: 8.0,
               ),
@@ -68,7 +77,7 @@ class _FeedWidgetState extends State<FeedWidget> {
               width: 16.0,
             ),
             Text(
-              '좋아요 100개',
+              '좋아요 ${widget.document['likedUseres']?.length ?? 0}개',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
             ),
           ],
@@ -94,33 +103,34 @@ class _FeedWidgetState extends State<FeedWidget> {
         SizedBox(
           height: 8.0,
         ),
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CommentPage(widget.document),
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      '댓글 $comment개 모두 보기',
-                      style: TextStyle(color: Colors.grey[500]),
-                    ),
-                  ],
+        if (commentCount > 0)
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CommentPage(widget.document),
                 ),
-                Text(widget.document['lastComment'] ?? ''),
-              ],
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        '댓글 $commentCount개 모두 보기',
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                    ],
+                  ),
+                  Text(widget.document['lastComment'] ?? ''),
+                ],
+              ),
             ),
           ),
-        ),
         Row(
           children: <Widget>[
             Expanded(
@@ -149,16 +159,40 @@ class _FeedWidgetState extends State<FeedWidget> {
   void _like() {
     // 기존 좋아요 리스트 가져오기
     final List likedUsers =
-        List<String>.from(widget.document['likedUsers'] ?? []);
+        List<String>.from(widget.document['likeUsers'] ?? []);
 
     // 나를 추가
     likedUsers.add(widget.user.email);
 
     // 업데이트 할 항목을 문서로 준비
+    final updateData = {
+      'likeUsers': likedUsers,
+    };
+
+    Firestore.instance
+        .collection('post')
+        .document(widget.document.documentID)
+        .updateData(updateData);
   }
 
   // 좋아요 취소
-  void _unlike() {}
+  void _unlike() {
+    final List likedUsers =
+        List<String>.from(widget.document['likeUsers'] ?? []);
+
+    // 나를 추가
+    likedUsers.remove(widget.user.email);
+
+    // 업데이트 할 항목을 문서로 준비
+    final updateData = {
+      'likeUsers': likedUsers,
+    };
+
+    Firestore.instance
+        .collection('post')
+        .document(widget.document.documentID)
+        .updateData(updateData);
+  }
 
   // 댓글 작성
   void _writeComment(String text) {}
